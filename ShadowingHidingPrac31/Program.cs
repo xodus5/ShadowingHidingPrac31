e9777dcb351ffb4226b800e61928de5ff04fd25d
TaeYeon <sudox913@gmail.com>
2023-06-12 13:41:07 +0900

    메서드 오버라이딩 예제

--- ShadowingHidingPrac31/Program.cs
+++ ShadowingHidingPrac31/Program.cs
@@ -15,6 +15,10 @@ namespace ShadowingHidingPrac31
             {
                 Console.WriteLine("부모의 메서드");
             }
+            public virtual void Method2() 
+            {
+                Console.WriteLine("부모의 메서드");
+            }
         }
 
         class Child : Parent
@@ -23,12 +27,15 @@ namespace ShadowingHidingPrac31
             public void Method()
             {
                 Console.WriteLine("자식의 메서드");
-
+            }
+            public override void Method2()
+            {
+                Console.WriteLine("부모의 메서드");
             }
         }
 
 
-            public static int number = 10;
+        public static int number = 10;
         static void Main(string[] args)
         {
             int number = 20;
@@ -38,8 +45,11 @@ namespace ShadowingHidingPrac31
             Parent p = child;
             Console.WriteLine(p.variable);
 
-                child.Method();
-                p.Method();
+            child.Method();
+            p.Method();
+
+            child.Method2();
+            p.Method2();
         }
     }
 }
