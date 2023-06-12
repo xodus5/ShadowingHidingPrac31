5bfdb18e5b2ff855fc34bc79ec87b53b222467b2
TaeYeon <sudox913@gmail.com>
2023-06-12 13:30:52 +0900

    Shadowing 예제

--- ShadowingHidingPrac31/Program.cs
+++ ShadowingHidingPrac31/Program.cs
@@ -8,8 +8,11 @@ namespace ShadowingHidingPrac31
 {
     internal class Program
     {
+        public static int number = 10;
         static void Main(string[] args)
         {
+            int number = 20;
+            Console.WriteLine(number);
         }
     }
 }
